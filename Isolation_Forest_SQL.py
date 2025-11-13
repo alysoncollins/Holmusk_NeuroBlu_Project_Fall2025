63e615cddd7a7395653bfa62eac274f87bc68f35
EliGubbins <112986003+EliGubbins@users.noreply.github.com>
2025-11-13 12:21:55 -0600

    Add selector to to use bmi or not & cleaned up main

--- Isolation_Forest_SQL.py
+++ Isolation_Forest_SQL.py
@@ -8,8 +8,10 @@ from sklearn.preprocessing import StandardScaler
 
 
 def main():
-     # select which measurement based on index
-    selector = 0
+    # select which measurement based on index
+    selector = 6
+    # BMI doesn't work for Glucose and A1c currently
+    use_bmi = 0
     # ['measurement', 'concept_id', 'lower', 'upper']
     measurements = [
         ['temperature', (4302666,), 95, 100.4], #0
@@ -27,89 +29,24 @@ def main():
     measurement, concept_id, lower, upper = measurements[selector]
 
     
+    query = query_function(use_bmi, concept_id, lower, upper)
 
-    query = f"""
-    WITH
-unit_concept_filter AS (
-    SELECT DISTINCT c.concept_id, c.concept_name
-    FROM concept c
-    INNER JOIN measurement m
-        ON c.concept_id = m.unit_concept_id
-    WHERE m.measurement_concept_id IN {concept_id}
-),
-gender_concept_filter AS (
-    SELECT concept_id, concept_name
-    FROM concept
-    WHERE concept_id IN (8507, 8532)
-),
-latest_bmi AS (
-    SELECT
-        m.person_id,
-        FIRST(m.value_as_number ORDER BY m.measurement_date DESC) AS bmi
-    FROM measurement m
-    WHERE m.measurement_concept_id = 4245997
-      AND m.value_as_number IS NOT NULL
-    GROUP BY m.person_id
-),
-deduped AS (
-    SELECT
-        ANY_VALUE(m.measurement_id) AS measurement_id,
-        m.person_id,
-        CAST(m.measurement_date AS DATE) AS measurement_date,
-        m.unit_concept_id,
-        AVG(m.value_as_number) AS value_as_number
-    FROM measurement m
-    JOIN unit_concept_filter ucf ON m.unit_concept_id = ucf.concept_id
-    WHERE m.value_as_number IS NOT NULL --AND m.value_as_number BETWEEN {lower} AND {upper}
-    GROUP BY m.person_id, CAST(m.measurement_date AS DATE), m.unit_concept_id
-)
-SELECT
-    d.measurement_id,
-    d.value_as_number,
-    CASE
-        WHEN sex_concept.concept_id = 8507 THEN 0
-        WHEN sex_concept.concept_id = 8532 THEN 1
-        ELSE 2
-    END AS sex,
-    EXTRACT(YEAR FROM d.measurement_date) - p.year_of_birth AS age,
-    d.unit_concept_id,
-    COALESCE(b.bmi, 21.7) AS bmi
-FROM deduped d
-JOIN person p ON d.person_id = p.person_id
-LEFT JOIN gender_concept_filter sex_concept ON p.gender_concept_id = sex_concept.concept_id
-LEFT JOIN latest_bmi b ON d.person_id = b.person_id
-    """
     
      # Run the query
     values = neuroblu.get_query(query)
     df = pl.DataFrame(values)
 
     # Filter out-of-range values
-    out_of_range = df.filter((pl.col("value_as_number") < lower) | (pl.col("value_as_number") > upper))
-    in_range = df.filter((pl.col("value_as_number") >= lower) & (pl.col("value_as_number") <= upper))
+    #in_range = df.filter((pl.col("value_as_number") >= lower) & (pl.col("value_as_number") <= upper))
+    in_range = df
 
-
-    # Save out-of-range values to CSV
-    #out_of_csv = f"{measurement}_out_of_range.csv"
-    out_of_range = out_of_range.select(["measurement_id", "value_as_number"])
-    #out_of_range.write_csv(out_of_csv)
-    #print(f"Saved {out_of_range.height} out-of-range rows to {out_of_csv}")
-
-    outlier_df_name = f"{measurement}_outlier_data_frame"
-    pd_outlier = out_of_range.to_pandas()
-    
-    neuroblu.save_df(pd_outlier, outlier_df_name, overwrite=True)
-    print(f"Saved {out_of_range.height} outliers to data frame")
-
+    #save_out_range(measurement, df, lower, upper)
     
     # (keep only value_as_number, age, sex)
     in_range = in_range.drop_nulls(subset=["value_as_number", "age", "sex"])
 
     # Select features
-    if measurement == 'body_mass_index':
-        X = in_range.select(["value_as_number", "age", "sex"])
-    else:
-        X = in_range.select(["value_as_number", "age", "sex", "bmi"])
+    X = in_range.select(["value_as_number", "age", "sex"])
 
     # Standardize (flatten) to avoid one feature dominating
     scaler = StandardScaler()
@@ -136,9 +73,18 @@ LEFT JOIN latest_bmi b ON d.person_id = b.person_id
 
     # save percentages
     percent = f"{measurement}_make_up.csv"
-    message = f"Detected {len(anomalies)} anomalies out of {len(df)} total samples."
+    message = f"Detected {anomalies.height} anomalies out of {len(df)} total samples."
     with open(percent, 'w', encoding='utf-8') as f:
         f.write(message)
+
+def save_out_range(measurement, df, lower, upper):
+    out_of_range = df.filter((pl.col("value_as_number") < lower) | (pl.col("value_as_number") > upper))
+    
+    outlier_df_name = f"{measurement}_outlier_data_frame"
+    pd_outlier = out_of_range.to_pandas()
+    
+    neuroblu.save_df(pd_outlier, outlier_df_name, overwrite=True)
+    print(f"Saved {out_of_range.height} outliers to data frame")
 
 
 
@@ -185,6 +131,105 @@ def graph_anomalies(measurement, scores):
     plt.tight_layout()
     plt.savefig(f"{measurement}_Decision_Score_Distribution.png", dpi=300)
     plt.close()
+
+def query_function(bmi, concept_id,lower,upper):
+    if bmi == 1:
+        query = f"""
+    WITH
+unit_concept_filter AS (
+    SELECT DISTINCT c.concept_id, c.concept_name
+    FROM concept c
+    INNER JOIN measurement m
+        ON c.concept_id = m.unit_concept_id
+    WHERE m.measurement_concept_id IN {concept_id}
+),
+gender_concept_filter AS (
+    SELECT concept_id, concept_name
+    FROM concept
+    WHERE concept_id IN (8507, 8532)
+),
+latest_bmi AS (
+    SELECT
+        m.person_id,
+        FIRST(m.value_as_number ORDER BY m.measurement_date DESC) AS bmi
+    FROM measurement m
+    WHERE m.measurement_concept_id = 4245997
+      AND m.value_as_number IS NOT NULL
+    GROUP BY m.person_id
+),
+deduped AS (
+    SELECT
+        ANY_VALUE(m.measurement_id) AS measurement_id,
+        m.person_id,
+        CAST(m.measurement_date AS DATE) AS measurement_date,
+        m.unit_concept_id,
+        AVG(m.value_as_number) AS value_as_number
+    FROM measurement m
+    JOIN unit_concept_filter ucf ON m.unit_concept_id = ucf.concept_id
+    WHERE m.value_as_number IS NOT NULL AND m.value_as_number BETWEEN {lower} AND {upper}
+    GROUP BY m.person_id, CAST(m.measurement_date AS DATE), m.unit_concept_id
+)
+SELECT
+    d.measurement_id,
+    d.value_as_number,
+    CASE
+        WHEN sex_concept.concept_id = 8507 THEN 0
+        WHEN sex_concept.concept_id = 8532 THEN 1
+        ELSE 2
+    END AS sex,
+    EXTRACT(YEAR FROM d.measurement_date) - p.year_of_birth AS age,
+    d.unit_concept_id,
+    COALESCE(b.bmi, 21.7) AS bmi
+FROM deduped d
+JOIN person p ON d.person_id = p.person_id
+LEFT JOIN gender_concept_filter sex_concept ON p.gender_concept_id = sex_concept.concept_id
+LEFT JOIN latest_bmi b ON d.person_id = b.person_id
+    """
+    elif bmi == 0:
+        query = f"""
+    WITH
+unit_concept_filter AS (
+    SELECT DISTINCT c.concept_id, c.concept_name
+    FROM concept c
+    INNER JOIN measurement m
+        ON c.concept_id = m.unit_concept_id
+    WHERE m.measurement_concept_id IN {concept_id}
+),
+gender_concept_filter AS (
+    SELECT concept_id, concept_name
+    FROM concept
+    WHERE concept_id IN (8507, 8532)
+),
+deduped AS (
+    SELECT
+        ANY_VALUE(m.measurement_id) AS measurement_id,
+        m.person_id,
+        CAST(m.measurement_date AS DATE) AS measurement_date,
+        m.unit_concept_id,
+        AVG(m.value_as_number) AS value_as_number
+    FROM measurement m
+    JOIN unit_concept_filter ucf ON m.unit_concept_id = ucf.concept_id
+    WHERE m.value_as_number IS NOT NULL AND m.value_as_number BETWEEN {lower} AND {upper}
+    GROUP BY m.person_id, CAST(m.measurement_date AS DATE), m.unit_concept_id
+)
+SELECT
+    d.measurement_id,
+    d.value_as_number,
+    CASE
+        WHEN sex_concept.concept_id = 8507 THEN 0
+        WHEN sex_concept.concept_id = 8532 THEN 1
+        ELSE 2
+    END AS sex,
+    EXTRACT(YEAR FROM d.measurement_date) - p.year_of_birth AS age,
+    d.unit_concept_id
+FROM deduped d
+JOIN person p ON d.person_id = p.person_id
+LEFT JOIN gender_concept_filter sex_concept ON p.gender_concept_id = sex_concept.concept_id
+    """
+    else:
+        print("initilization not set up properly")
+        quit()
+    query
 
 if __name__ == "__main__":
     main()
